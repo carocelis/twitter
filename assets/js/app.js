b5e63b451968b91f00c69f8a141228c27ed351e6
carocelis <carolina.celis.sepulveda@gmail.com>
2017-11-08 02:14:25 -0300

    version 0.0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,32 @@
 function add(){
 	var comentado = document.getElementById("comentario").value;
-	document.getElementById("comentario").value = ""; //limpio el área de texto después de enviado el twitt
+	
+	if(comentado == "" || comentado.length > 140){
+		var boton = document.getElementById("tweet");
+		tweet.enable = false;
+	}else{	
+		document.getElementById("comentario").value = ""; //limpio el área de texto después de enviado el twitt
+		var nuevoComentario = document.createElement("div");
+		var nuevoComentarioP = document.createElement("p");
+		var fecha = document.createElement("span");
+		var contenedor = document.getElementById("lineaTiempo");
 
-	var nuevoComentario = document.createElement("div");
-	var nuevoComentarioP = document.createElement("p");
-	var fecha = document.createElement("span");
-	var contenedor = document.getElementById("lineaTiempo");
-
-	nuevoComentario.appendChild(nuevoComentarioP);
-	nuevoComentario.appendChild(fecha);
-	contenedor.appendChild(nuevoComentario);
-	nuevoComentarioP.textContent = comentado;
-	/*fecha.textContent = new Date();*/
-
+		nuevoComentario.appendChild(nuevoComentarioP);
+		nuevoComentario.appendChild(fecha);
+		contenedor.appendChild(nuevoComentario);
+		nuevoComentarioP.textContent = comentado;
+		fecha.textContent = moment().format('h:mm a');
+	}
 }
+function contar() { 
+    var max = "140"; 
+    var cadena = document.getElementById("comentario").value; 
+    var long = cadena.length; 
+    if(long <= max) { 
+         document.getElementById("contador").value = max-long;
+    }else{ 
+         document.getElementById("comentario").value = cadena.substr(0, max);
+    } 
+
+} 
+
